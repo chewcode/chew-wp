85b1a5ca4980ddf7f6fb55b0d6303b0dc3a642ce
Ben Bowler <ben.bowler@vice.com>
2013-11-24 12:51:54 +0000

    Chew WP Embed Plugin

---
+++ chew-wp.php
@@ -0,0 +1,43 @@
+<?php
+/*
+Plugin Name: Chew - Video Embed Shortcode
+Plugin URI: http://chew.tv/chew-wp
+Description: Chew is a Live and VOD platform for Artists, DJs and Labels. Embed Chew.tv videos into your wordpress site using the short tag [chew video="UX7NNO5DHUG2" height="500px" width="300px" autoplay="yes"]
+Version: 0.1.3
+Author: Ben Bowler
+Author URI: http://chew.tv
+*/
+
+/*
+Chew Embed Shortcode Paul McKnight at http://www.reallyeffective.co.uk
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// Register shortcode
+add_shortcode("chew", "chew_player");
+
+function chew_player($atts) {
+	extract( shortcode_atts( array( // Array of defaults
+		'video' => '',
+		'width' => '600px',
+		'height' => '400px',
+		'autoplay' => 'no',
+	), $atts ) );
+
+	//process plugin
+	$demolp_output = "<iframe src=\"http://chew.tv/embed/?video={$video}&autoplay={$autoplay}\" height=\"{$height}\" width=\"{$width}\" allowfullscreen=\"\" frameborder=\"0\"></iframe>";
+	//send back text to calling function
+	return $demolp_output;
+}
